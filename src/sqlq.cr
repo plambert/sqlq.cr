fc88b4bc93b60ad469392caa020e35c16a9167a4
Paul M. Lambert <plambert@plambert.net>
2025-07-02 20:23:42 -0700

    allow multiple queues per file

--- src/sqlq.cr
+++ src/sqlq.cr
@@ -3,6 +3,7 @@ require "sqlite3"
 
 module Queue
   VERSION            = "0.1.0"
+  DEFAULT_QUEUE_NAME = "default_queue"
   DEFAULT_QUEUE_FILE = "~/.cache/sqlq/default_queue"
 
   enum Command
@@ -24,12 +25,14 @@ module Queue
     setter id_format : String? = nil
     property cmd : Command
     property timezone : Time::Location
+    property queue_name : String
 
     def initialize(@argv = ARGV.dup)
       opts = @argv.dup
       @arguments = [] of String
       @cmd = Command::Help
       @timezone = Time::Location.local
+      @queue_name = DEFAULT_QUEUE_NAME
       @dbfile = ""
       while opt = opts.shift?
         case opt
@@ -41,6 +44,8 @@ module Queue
           @timezone = Time::Location.local
         when "--utc"
           @timezone = Time::Location::UTC
+        when "--queue", "--queue-name", "-q"
+          @queue_name = opts.shift? || raise ArgumentError.new "#{opt}: expected an argument"
         when .starts_with? '-'
           raise ArgumentError.new "#{opt}: unknown option"
         when "add"
@@ -83,7 +88,7 @@ module Queue
       parent = Path[@dbfile].parent
       Dir.mkdir_p parent unless File.directory? parent
       @db = DB.open "sqlite3:#{@dbfile}?max_idle_pool_size=3&initial_pool_size=3&journal_mode=wal&busy_timeout=5000"
-      @db.exec "CREATE TABLE IF NOT EXISTS queue (id INTEGER PRIMARY KEY, entry TEXT NOT NULL, creation_time TEXT NOT NULL)"
+      @db.exec "CREATE TABLE IF NOT EXISTS \"#{@queue_name}\" (id INTEGER PRIMARY KEY, entry TEXT NOT NULL, creation_time TEXT NOT NULL)"
     end
 
     def run
@@ -112,7 +117,7 @@ module Queue
     end
 
     def create_id_format
-      max = @db.query_one "SELECT MAX(id) FROM queue", as: Int64
+      max = @db.query_one "SELECT MAX(id) FROM \"#{@queue_name}\"", as: Int64
       "%" + max.to_s.size.to_s + "d"
     end
 
@@ -122,7 +127,7 @@ module Queue
       else
         creation_time = Time.local(location: @timezone)
         @arguments.each do |entry|
-          @db.exec "INSERT INTO queue (entry, creation_time) VALUES (?, ?)", entry, creation_time
+          @db.exec "INSERT INTO \"#{@queue_name}\" (entry, creation_time) VALUES (?, ?)", entry, creation_time
         end
         if 1 == @arguments.size
           puts "1 entry added"
@@ -135,9 +140,9 @@ module Queue
     def cmd_list
       limit : Int64? = @arguments[0]? && @arguments[0].to_i64?
       if limit
-        sql = "SELECT id, creation_time, entry FROM queue ORDER BY creation_time, id LIMIT #{limit}"
+        sql = "SELECT id, creation_time, entry FROM \"#{@queue_name}\" ORDER BY creation_time, id LIMIT #{limit}"
       else
-        sql = "SELECT id, creation_time, entry FROM queue ORDER BY creation_time, id"
+        sql = "SELECT id, creation_time, entry FROM \"#{@queue_name}\" ORDER BY creation_time, id"
       end
       entries = @db.query_all sql, as: {Int64, Time, String}
       entries.each do |entry|
@@ -146,7 +151,7 @@ module Queue
     end
 
     def cmd_take
-      if deleted_entry = @db.query_one? "DELETE FROM queue RETURNING entry ORDER BY creation_time, id LIMIT 1", as: String
+      if deleted_entry = @db.query_one? "DELETE FROM \"#{@queue_name}\" RETURNING entry ORDER BY creation_time, id LIMIT 1", as: String
         puts deleted_entry
       else
         STDERR.puts "no entries"
@@ -155,7 +160,7 @@ module Queue
     end
 
     def cmd_peek
-      if next_entry = @db.query_one? "SELECT entry FROM queue ORDER BY creation_time, id LIMIT 1", as: String
+      if next_entry = @db.query_one? "SELECT entry FROM \"#{@queue_name}\" ORDER BY creation_time, id LIMIT 1", as: String
         puts next_entry
       else
         STDERR.puts "no entries"
@@ -271,9 +276,9 @@ module Queue
         end
 
         sql = if ENV["SQLQ_DEBUG"]?
-                "SELECT id, creation_time, entry FROM queue WHERE " + where_clauses.join(" AND ") + " ORDER BY creation_time, id"
+                "SELECT id, creation_time, entry FROM \"#{@queue_name}\" WHERE " + where_clauses.join(" AND ") + " ORDER BY creation_time, id"
               else
-                "DELETE FROM queue WHERE " + where_clauses.join(" AND ") + " RETURNING id, creation_time, entry"
+                "DELETE FROM \"#{@queue_name}\" WHERE " + where_clauses.join(" AND ") + " RETURNING id, creation_time, entry"
               end
 
         results = @db.query_all sql, args: where_args, as: {Int64, Time, String}
